53a5c713780babdbe2ef9b50ec4fd9b840e59736
zhenli8710 <438580507@qq.com>
2026-06-19 23:09:21 +0800

    fix: stock data APIs

---
+++ push.bat
@@ -0,0 +1,11 @@
+﻿# Push stock-mini-app changes to GitHub
+
+# 1. Navigate to repo
+cd C:\Users\Administrator\stock-mini-app-repo
+
+# 2. Push to GitHub
+git push origin master
+
+# If HTTPS doesn't work, try SSH:
+# git remote set-url origin git@github.com:zhenli8710/-stock-mini-app.git
+# git push origin master
